2dc209849c5dd04a65763363049b1989ca96cdd2
Whilker Henrique dos Santos Silva <77589533+Whilker-Silva@users.noreply.github.com>
2024-04-30 01:21:58 -0300

    atualização cmakelist

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,15 @@
 #include <iostream>
 #include <cstdlib>
-#include "Containers/Registro/Registro.h"
-#include "Containers/Control/Control.h"
-#include "Utils/Serial/Serial.h"
+
+#include "Containers/Controle/Controle.h"
+
 
 using namespace std;
 
 int main (int argc, char* argv[])  /// < Main function that create and calls for reading and writing in memory
 {
-    Serial *serial = new Serial(14, 15, 10, 9600);
-    
-    int i = 0;
-    while(i<1000){
-        string x = "hellor arudino " + to_string(i);
-        serial->envia(x);
-        delay(50);
-        i++;
-    }
-    
-    // Registro *registro = new Registro();
+    Controle *controle = new Controle();   
+   
 
     int quit = -1;
     do
@@ -27,7 +18,7 @@ int main (int argc, char* argv[])  /// < Main function that create and calls for
     }
     while (quit != 0);
 
-    //delete registro;
+    delete controle;
 
     return EXIT_SUCCESS;
 }
