4a5ab65dd6a8d7ee54dd312d634734dddf2ca957
Nucleo-LMT <167372071+Nucleo-LMT@users.noreply.github.com>
2024-07-02 00:44:45 -0300

    Configurando workspace ROS

---
+++ Threads/build/CMakeFiles/vida.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/odroid/Documentos/Plataforma-VIDA/Threads/src/Containers/Control/Control.cpp" "CMakeFiles/vida.dir/Containers/Control/Control.cpp.o" "gcc" "CMakeFiles/vida.dir/Containers/Control/Control.cpp.o.d"
+  "/home/odroid/Documentos/Plataforma-VIDA/Threads/src/Containers/Encoder/Encoder.cpp" "CMakeFiles/vida.dir/Containers/Encoder/Encoder.cpp.o" "gcc" "CMakeFiles/vida.dir/Containers/Encoder/Encoder.cpp.o.d"
+  "/home/odroid/Documentos/Plataforma-VIDA/Threads/src/Containers/Record/Record.cpp" "CMakeFiles/vida.dir/Containers/Record/Record.cpp.o" "gcc" "CMakeFiles/vida.dir/Containers/Record/Record.cpp.o.d"
+  "/home/odroid/Documentos/Plataforma-VIDA/Threads/src/Containers/Steering/Steering.cpp" "CMakeFiles/vida.dir/Containers/Steering/Steering.cpp.o" "gcc" "CMakeFiles/vida.dir/Containers/Steering/Steering.cpp.o.d"
+  "/home/odroid/Documentos/Plataforma-VIDA/Threads/src/Utils/PosixShMem/PosixShMem.cpp" "CMakeFiles/vida.dir/Utils/PosixShMem/PosixShMem.cpp.o" "gcc" "CMakeFiles/vida.dir/Utils/PosixShMem/PosixShMem.cpp.o.d"
+  "/home/odroid/Documentos/Plataforma-VIDA/Threads/src/Utils/Serial/Serial.cpp" "CMakeFiles/vida.dir/Utils/Serial/Serial.cpp.o" "gcc" "CMakeFiles/vida.dir/Utils/Serial/Serial.cpp.o.d"
+  "/home/odroid/Documentos/Plataforma-VIDA/Threads/src/Utils/ThreadBase/ThreadBase.cpp" "CMakeFiles/vida.dir/Utils/ThreadBase/ThreadBase.cpp.o" "gcc" "CMakeFiles/vida.dir/Utils/ThreadBase/ThreadBase.cpp.o.d"
+  "/home/odroid/Documentos/Plataforma-VIDA/Threads/src/Utils/UDP/UDP.cpp" "CMakeFiles/vida.dir/Utils/UDP/UDP.cpp.o" "gcc" "CMakeFiles/vida.dir/Utils/UDP/UDP.cpp.o.d"
+  "/home/odroid/Documentos/Plataforma-VIDA/Threads/src/main.cpp" "CMakeFiles/vida.dir/main.cpp.o" "gcc" "CMakeFiles/vida.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
