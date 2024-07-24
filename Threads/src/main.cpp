2a47a96bbedf23f22f710854ece2a6c2c8086606
Nucleo-LMT <167372071+Nucleo-LMT@users.noreply.github.com>
2024-07-24 18:45:53 -0300

    Compilação nó control

--- Threads/src/main.cpp
+++ Threads/src/main.cpp
@@ -10,9 +10,9 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     Control *control = new Control();
-    Encoder *encoder = new Encoder();
-    Steering *steering = new Steering();
-    Record *registro = new Record();
+    //Encoder *encoder = new Encoder();
+    //Steering *steering = new Steering();
+    //Record *registro = new Record();
 
     int quit = -1;
     do
@@ -21,9 +21,9 @@ int main(int argc, char *argv[])
     } while (quit != 0);
 
     delete control;
-    delete encoder;
-    delete steering;
-    delete registro;
+    //delete encoder;
+    //delete steering;
+    //delete registro;
 
     return EXIT_SUCCESS;
 }
